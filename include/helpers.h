899b2277a9b54a105681e7d786d8aec6ef43d943
Albert Cornelissen <acornelissen@users.noreply.github.com>
2025-02-27 22:00:56 +0000

    Initial commit

---
+++ include/helpers.h
@@ -0,0 +1,109 @@
+
+
+// Helper functions
+// ---------------------
+
+void loadPrefs()
+{
+  prefs.begin("mrf", false);
+
+  if (prefs.isKey("iso")) {
+    iso = prefs.getInt("iso", 400);
+    iso_index = prefs.getInt("iso_index", 5);
+
+    byte tempLenses[sizeof(lenses)];
+    prefs.getBytes("lenses", tempLenses, sizeof(lenses));
+    memcpy(lenses, tempLenses, sizeof(lenses));
+
+    selected_lens = prefs.getInt("selected_lens", 1);
+    selected_diopter = prefs.getInt("selected_diopter", 0);
+
+    aperture = prefs.getFloat("aperture", APERTURES[0]);
+    aperture_index = prefs.getInt("aperture_index", 0);
+
+    lgmode = prefs.getBool("lgmode", false);
+
+  }
+
+  prefs.end();
+}
+
+void savePrefs()
+{
+  prefs.begin("mrf", false);
+  prefs.putInt("iso", iso);
+  prefs.putInt("iso_index", iso_index);
+  prefs.putFloat("aperture", aperture);
+  prefs.putInt("aperture_index", aperture_index);
+  prefs.putInt("selected_lens", selected_lens);
+  prefs.putInt("selected_diopter", selected_diopter);
+  prefs.putBool("lgmode", lgmode);
+  prefs.putBytes("lenses", (byte *)lenses, sizeof(lenses));
+  prefs.end();
+}
+
+
+String cmToReadable(int cm)
+{
+
+  if (cm < 100)
+  {
+    return String(cm) + "cm";
+  }
+  else
+  {
+    return String(float(cm) / 100, 1) + "m";
+  }
+}
+
+int calcMovingAvg(int index, int sensorVal)
+{
+  int readIndex = curReadIndex[index];
+  sampleTotal[index] = sampleTotal[index] - (samples[index][readIndex]);
+
+  samples[index][readIndex] = sensorVal;
+  sampleTotal[index] = sampleTotal[index] + samples[index][readIndex];
+  curReadIndex[index] = curReadIndex[index] + 1;
+
+  if (curReadIndex[index] >= SMOOTHING_WINDOW_SIZE)
+  {
+    curReadIndex[index] = 0;
+  }
+
+  sampleAvg[index] = sampleTotal[index] / SMOOTHING_WINDOW_SIZE;
+  return sampleAvg[index];
+}
+
+int_fast16_t getFocusRadius()
+{
+  int minRadius = 2;
+  int maxRadius = 40;
+
+  int radius = min(maxRadius, max(minRadius, abs(distance_display - lens_distance_raw)));
+
+  return radius;
+}
+
+
+// Function to compute the constants a and b for d = a * ln(r) + b
+typedef struct {
+  float a;
+  float b;
+} VCNL4040LogFunctionConstants;
+
+VCNL4040LogFunctionConstants computeLogConstants(int r1, float d1, int r2, float d2) {
+  VCNL4040LogFunctionConstants constants;
+  
+  if (r1 <= 0 || r2 <= 0 || r1 == r2) {
+      constants.a = 0;
+      constants.b = 0;
+      return constants; // Return zeros for invalid input
+  }
+  
+  // Solve for a and b
+  constants.a = (d2 - d1) / (log(r2) - log(r1));
+  constants.b = d1 - constants.a * log(r1);
+  
+  return constants;
+}
+// ---------------------
---
+++ include/setfuncs.h
@@ -0,0 +1,187 @@
+#include <cmath>
+
+// Functions to read values from sensors and set variables
+// ---------------------
+void setDistance()
+{
+
+  // Get data from the sensor
+  tfluna.getData(distance);
+
+  if (distance)
+  { // Get data from Lidar
+    distance_display = distance + LIDAR_OFFSET;
+    if ( distance_display != prev_distance)
+    {
+      if ( distance_display >= DISTANCE_MAX)
+      {
+         distance_cm = "> " + String(DISTANCE_MAX / 100) + "m";
+      }
+      else if ( distance_display < DISTANCE_MIN)
+      {
+        distance_cm = "< " + String(DISTANCE_MIN) + "cm";
+      }
+      else {
+        distance_cm = cmToReadable( distance_display);
+      }
+      prev_distance =  distance_display;
+    }
+  }
+  else {
+    distance_cm = "...";
+  }
+}
+
+
+int getLensSensorReading()
+{
+  int sensorVal = ads1115.readADC_SingleEnded(LENS_ADC_PIN);
+  return calcMovingAvg(0, sensorVal) / 10;
+}
+
+void setLensDistance(bool force_refresh = false)
+{
+
+  // Get the lens sensor reading and constrain it to the min and max values
+  lens_sensor_reading = constrain(getLensSensorReading(), lenses[selected_lens].sensor_reading[0], lenses[selected_lens].sensor_reading[1]);
+  
+  
+  // Perform linear interpolation to get the lens distance in mm, and round to 1 decimal place
+  float lens_movement_mm = map(lens_sensor_reading, lenses[selected_lens].sensor_reading[0], lenses[selected_lens].sensor_reading[1], LENS_MOVEMENT_MM_MIN * 1000, LENS_MOVEMENT_MM_MAX * 1000) / 1000.00;
+  lens_movement_mm = (LENS_MOVEMENT_MULTIPLIER * lens_movement_mm) / LENS_MOVEMENT_MM_MAX;
+
+  if (lgmode) {
+    lens_movement_mm = (LOMOGRAFLOK_OFFSET * lens_movement_mm) / LENS_MOVEMENT_MM_MAX;
+    lens_movement_mm = lens_movement_mm + LOMOGRAFLOK_OFFSET;
+  }
+
+
+  lens_distance_raw = 9999999;
+  if (lens_movement_mm > 0) {
+    lens_distance_raw = ((lenses[selected_lens].focal_length * lenses[selected_lens].focal_length + 2 * lens_movement_mm * lenses[selected_lens].focal_length) / lens_movement_mm) / 10;
+  
+    if (selected_diopter > 0) {
+     lens_distance_raw = 1.0 / ((1.0 / lens_distance_raw) + (selected_diopter / 100.0));
+    }
+  }
+
+  if (lens_sensor_reading != prev_lens_sensor_reading || force_refresh) {
+    
+    if (abs(lens_sensor_reading - prev_lens_sensor_reading) > 2) {
+        lastActivityTime = millis();
+    }
+    
+    prev_lens_sensor_reading = lens_sensor_reading;
+    if (lens_sensor_reading  <= lenses[selected_lens].sensor_reading[1]) {
+      lens_sensor_reading  = lenses[selected_lens].sensor_reading[1];
+    }
+        
+    else if (lens_sensor_reading  >= lenses[selected_lens].sensor_reading[0]) {
+      lens_sensor_reading  = lenses[selected_lens].sensor_reading[0];
+    }
+
+  if (lens_distance_raw >=  9999999 && lgmode == false && selected_diopter == 0) {
+        lens_distance_cm = "Inf.";
+    }
+    else {
+      lens_distance_cm = cmToReadable(lens_distance_raw);
+    }
+  }
+   
+}
+
+void setVoltage()
+{
+  bat_per = maxlipo.cellPercent();
+  if (bat_per > 100)
+  {
+    bat_per = 100;
+  }
+
+  if (bat_per != prev_bat_per)
+  {
+    prev_bat_per = bat_per;
+  }
+}
+
+void setLightMeter()
+{
+  lux = lightMeter.readLightLevel();
+
+  if (lux != prev_lux)
+  {
+    prev_lux = lux;
+    if (lux <= 0)
+    {
+      shutter_speed = "Dark!";
+    }
+    else
+    {
+
+      if (aperture == 0)
+      {
+        cycleApertures("up");
+      }
+
+      float speed = round(((aperture * aperture) * K) / (lux * iso) * 1000.0) / 1000.0;
+
+      struct SpeedRange
+      {
+        float lower;
+        float upper;
+        const char *print_speed_range;
+      };
+
+      SpeedRange speed_ranges[] = {
+          {0.001, 0.002, "1/1000"},
+          {0.002, 0.004, "1/500"},
+          {0.004, 0.008, "1/250"},
+          {0.008, 0.016, "1/125"},
+          {0.016, 0.033, "1/60"},
+          {0.033, 0.066, "1/30"},
+          {0.066, 0.125, "1/15"},
+          {0.125, 0.250, "1/8"},
+          {0.250, 0.500, "1/4"},
+          {0.500, 1, "1/2"}};
+
+      char print_speed[10];
+      dtostrf(speed, 4, 1, print_speed);
+
+      for (int i = 0; i < sizeof(speed_ranges) / sizeof(speed_ranges[0]); i++)
+      {
+        if (speed_ranges[i].lower <= speed && speed < speed_ranges[i].upper)
+        {
+          strcpy(print_speed, speed_ranges[i].print_speed_range);
+          break;
+        }
+      }
+
+     
+      if (speed >= 1)
+      {
+        char print_speed_raw[10];
+        dtostrf(speed, 4, 2, print_speed_raw);
+        shutter_speed = strcat(print_speed_raw, " sec.");
+      }
+      else {
+         shutter_speed = strcat(print_speed, " sec.");
+      }
+       
+    }
+  }
+}
+
+void toggleLidar()
+{
+  if (sleepMode && lidarOn)
+  {
+    tfluna.sendCommand(DISABLE_OUTPUT, 0);
+    lidarOn = false;
+  }
+  else if (!sleepMode && !lidarOn)
+  {
+    tfluna.sendCommand(ENABLE_OUTPUT, 0);
+    lidarOn = true;
+  }
+}
+// ---------------------
